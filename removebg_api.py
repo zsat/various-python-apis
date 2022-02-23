9a7f74fde1520e807c279fcfefc23e6b0a356f8f
zack <69357099+zsat@users.noreply.github.com>
2022-02-22 20:35:16 -0500

    fixed indentation

--- removebg_api.py
+++ removebg_api.py
@@ -3,23 +3,23 @@
 
 class removebg_api:
 	
-	def removebg(self, path_to_img):
+    def removebg(self, path_to_img):
     
     storage_path = '~/path/to/storage/' # path we want to store our finished images in
     suffix = str.lower(path_to_image.split('.')[-1]) # getting suffix so we can produce a separate file
         
-		#	if this throws an exception then there's likely a problem opening the file, perhaps not a valid format
-		try:
-			response = requests.post(
+	# if this throws an exception then there's likely a problem opening the file, perhaps not a valid format
+	try:
+		response = requests.post(
     	'https://api.remove.bg/v1.0/removebg',
     	files={'image_file': open(path_to_img, 'rb')},
     	data={'size': 'auto'},
-   	  headers={'X-Api-Key': 'YOUR-API-KEY'}, # follow the instructions here: https://www.remove.bg/api#remove-background
+   	  	headers={'X-Api-Key': 'YOUR-API-KEY'}, # follow the instructions here: https://www.remove.bg/api#remove-background
     	)
-		except Exception as e:
-			return e # returns as string
+	except Exception as e:
+		return e # returns as string
     
-		# if all goes well, write the image to our designated storage path
+	# if all goes well, write the image to our designated storage path
     if response.status_code == requests.codes.ok:
         with open(storage_path + 'no-bg.'+suffix, 'wb') as out:
             out.write(response.content)
